4b196dc5c52ee924dd2ea8068a4f39d0de320c71
Gordon JC Pearce <gordon@gjcp.net>
2011-01-12 23:50:13 +0000

    run a hook program when tuning

--- lysdr.c
+++ lysdr.c
@@ -27,15 +27,68 @@ sdr_data_t *sdr;
 static gboolean connect_input;
 static gboolean connect_output;
 static gint centre_freq;
+static gchar *tuning_hook;
 
 static GOptionEntry opts[] = 
 {
   { "ci", 0, 0, G_OPTION_ARG_NONE, &connect_input, "Autoconnect input to first two jack capture ports", NULL },
   { "co", 0, 0, G_OPTION_ARG_NONE, &connect_output, "Autoconnect output to first two jack playback ports", NULL },
   { "freq", 'f', 0, G_OPTION_ARG_INT, &centre_freq, "Set the centre frequency in Hz", NULL},
+  { "tuning-hook", 0, 0, G_OPTION_ARG_STRING, &tuning_hook, "Program to run when tuned frequency changes", NULL },
   { NULL }
 };
 
+static void hook_setup(gpointer data) {
+    GString *s = g_string_new("");
+    gint tuning = (gint)gtk_adjustment_get_value(GTK_ADJUSTMENT(sdr->tuning));
+
+    g_string_printf(s, "%d", sdr->centre_freq);
+    g_setenv("LYSDR_CENTRE", s->str, TRUE);
+
+    g_string_printf(s, "%d", tuning);
+    g_setenv("LYSDR_OFFSET", s->str, TRUE);
+
+    g_string_printf(s, "%d", sdr->centre_freq + tuning);
+    g_setenv("LYSDR_FREQ", s->str, TRUE);
+
+    switch (sdr->mode) {
+        case SDR_LSB:
+            g_string_printf(s, "LSB");
+            break;
+        case SDR_USB:
+            g_string_printf(s, "USB");
+            break;
+    }
+    g_setenv("LYSDR_MODE", s->str, TRUE);
+
+    g_string_free(s, TRUE);
+}
+
+static gboolean run_hook(gpointer data) {
+    gchar *hook = (gchar *)data;
+
+    if (hook != NULL) {
+        GPid pid;
+        gchar *argv[] = { hook, NULL };
+
+        g_spawn_async(NULL, argv, NULL, 0, hook_setup, NULL, &pid, NULL);
+        g_spawn_close_pid(pid);
+    }
+
+    return FALSE;
+}
+
+static guint tuning_hook_timeout = 0;
+
+static void tuning_changed(GtkAdjustment *adjustment, gpointer data) {
+    if (tuning_hook_timeout) {
+        // Cancel if already scheduled: we want to only run the hook once
+        // the value's settled.
+        g_source_remove(tuning_hook_timeout);
+    }
+
+    tuning_hook_timeout = g_timeout_add(100, run_hook, (gpointer)tuning_hook);
+}
 
 int main(int argc, char *argv[]) {
     GError *error = NULL;
@@ -76,6 +129,8 @@ int main(int argc, char *argv[]) {
     sdr->centre_freq = centre_freq;
 
     gui_display(sdr);
+
+    gtk_signal_connect(GTK_OBJECT(sdr->tuning), "value-changed", G_CALLBACK(tuning_changed), NULL);
 
     gtk_adjustment_set_value(GTK_ADJUSTMENT(sdr->tuning), 0);
 
